e0efc5d464ef0367bb003e83b5f48315d097d85f
smyrkh <190591493+smyrkh@users.noreply.github.com>
2026-06-27 19:27:57 +0900

    feat: delete trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  trailingSlash: "never",
+  build: {
+    format: "file",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
